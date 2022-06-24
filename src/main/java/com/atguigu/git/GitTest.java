7397b987e358b791b67ec37e5f6a141be8535870
root <1547989756@qq.com>
2022-06-24 16:10:36 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hollo.git");
+        System.out.println("push.git");
     }
 }
